79e21029f45471391bd0f40a7f984cee202458b5
riyandrmd <ryanda.rama10@gmail.com>
2023-06-29 12:26:16 +0700

    first

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Jaya Jaya Jaya</div>
+    <div> 
+        <cite>
+        Bahwa sesungguhnya Kemerdekaan itu ialah hak
+        segala bangsa dan oleh sebab itu, maka penjajahan
+        di atas dunia harus dihapuskan, karena tidak sesuai
+        dengan peri-kemanusiaan dan peri-keadilan.
+        </cite>
+    </div>
   )
 }
 
